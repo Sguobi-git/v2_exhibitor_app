630be8177362628a5545adfdfb303f177205d5c7
Sguobi-git <guobisouhail@gmail.com>
2025-05-13 14:10:33 -0400

    Update Intro.py

--- v2_exhibitor_app/Intro.py
+++ v2_exhibitor_app/Intro.py
@@ -120,21 +120,23 @@ body {{
 }}
 
 .video-container {{
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    z-index: -1;
 }}
 
 video {{
-    position: absolute;
-    top: 0;
-    left: 0;
     width: 100vw;
     height: 100vh;
     object-fit: cover;
+    margin: 0;
+    padding: 0;
+    display: block;
 }}
 
 .top-left-container {{
